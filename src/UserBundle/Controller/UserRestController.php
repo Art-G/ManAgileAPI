c53634031c88a23966ebeb300646586c8af004cc
Arthur Gaudard <gaudard.arthur@gmail.com>
2015-11-07 19:04:51 +0100

    Nouvelle route /api/me

--- src/UserBundle/Controller/UserRestController.php
+++ src/UserBundle/Controller/UserRestController.php
@@ -8,11 +8,38 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class UserRestController extends Controller
 {
-	public function getUserAction($username){
-		$user = $this->getDoctrine()->getRepository('UserBundle:User')->findOneByUsername($username);
-		if(!is_object($user)){
-			throw $this->createNotFoundException();
-		}
-		return $user;
-	}
+
+    /**
+    *
+    * @param type $username
+    *
+    * @View(serializerGroups={"Default","Details"})
+    */
+    public function getUserAction($username){
+        $user = $this->getDoctrine()->getRepository('UserBundle:User')->findOneByUsername($username);
+        if(!is_object($user)){
+            throw $this->createNotFoundException();
+        }
+        return $user;
+    }
+
+    /**
+    *
+    * @View(serializerGroups={"Me"})
+    */
+    public function getMeAction(){
+        $this->forwardIfNotAuthenticated();
+        return $this->getUser();
+    }
+
+    /**
+    * Shortcut to throw a AccessDeniedException($message) if the user is not authenticated
+    * @param String $message The message to display (default:'warn.user.notAuthenticated')
+    */
+    protected function forwardIfNotAuthenticated($message='warn.user.notAuthenticated'){
+        if (!is_object($this->getUser()))
+        {
+            throw new AccessDeniedException($message);
+        }
+    }
 }
--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -5,10 +5,16 @@ namespace UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+use JMS\Serializer\Annotation\Groups;
+use JMS\Serializer\Annotation\VirtualProperty;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
+ *
+ * @ExclusionPolicy("all")
  */
 class User extends BaseUser
 {
@@ -16,8 +22,136 @@ class User extends BaseUser
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
+     * @Expose
+     * @Groups({"Me","Details"})
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     * @Groups({"Me","Details"})
+     */
+    protected $username;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     * @Groups({"Details"})
+     */
+    protected $usernameCanonical;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     * @Groups({"Me","Details"})
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     * @Groups({"Details"})
+     */
+    protected $emailCanonical;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Expose
+     * @Groups({"Me","Details"})
+     */
+    protected $enabled;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     * @Groups({"Me","Details"})
+     */
+    protected $salt;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     * @Groups({"Me","Details"})
+     */
+    protected $password;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     * @Groups({"Details"})
+     */
+    protected $plainPassword;
+
+    /**
+     * @ORM\Column(type="DateTime")
+     * @Expose
+     * @Groups({"Me","Details"})
+     */
+    protected $lastLogin;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     * @Groups({"Me","Details"})
+     */
+    protected $confirmationToken;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     * @Groups({"Details"})
+     */
+    protected $passwordRequestedAt;
+
+    /**
+     * @ORM\Column(type="Collection")
+     * @Expose
+     * @Groups({"Me","Details"})
+     */
+    protected $groups;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Expose
+     * @Groups({"Details"})
+     */
+    protected $locked;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Expose
+     * @Groups({"Details"})
+     */
+    protected $expired;
+
+    /**
+     * @ORM\Column(type="DateTime")
+     * @Expose
+     * @Groups({"Details"})
+     */
+    protected $expiresAt;
+
+    /**
+     * @ORM\Column(type="array")
+     * @Expose
+     * @Groups({"Me","Details"})
+     */
+    protected $roles;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Expose
+     * @Groups({"Details"})
+     */
+    protected $credentialsExpired;
+
+    /**
+     * @ORM\Column(type="DateTime")
+     * @Expose
+     * @Groups({"Details"})
+     */
+    protected $credentialsExpireAt;
 
     public function __construct()
     {
